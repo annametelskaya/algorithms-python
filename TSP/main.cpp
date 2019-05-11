198890580b1e17832ee046bcd179a7bc43c519c5
annametelskaya <annam.2620@gmail.com>
2019-05-11 09:54:39 +0300

    update tsp

--- TSP/main.cpp
+++ TSP/main.cpp
@@ -1,5 +1,4 @@
 #include <time.h>
-//#include <bits/stdc++.h>
 #include "tsp.h"
 
 Graph::Graph(int size) {
@@ -14,7 +13,7 @@ Graph::Graph(int size) {
             if (j == i)
                 graph[i][j] = 0;
             else {
-                int randomNumber = 1 + rand() % 14;
+                int randomNumber = 1 + rand() % 100;
                 graph[i][j] = randomNumber;
                 graph[j][i] = randomNumber;
             }
@@ -38,15 +37,15 @@ void printGraph(Graph graph) {
     }
 }
 
-void getPathWeight(int **graph, vector<int> path) {
+int getPathWeight(int **graph, vector<int> path) {
     int weight = 0;
     int prev_point = path[0];
     for (int i : path) {
         weight += graph[prev_point][i];
         prev_point = i;
     }
-    weight += graph[path[0]][path[path.size()-1]];
-    cout << "Weight: " << weight;
+    weight += graph[path[0]][path[path.size() - 1]];
+    return weight;
 }
 
 bool checkVertexInIndex(const vector<int> &path, int j) {
@@ -76,17 +75,43 @@ vector<int> findPath(int **graph, int size) {
     return path;
 }
 
+void changePath(Graph graph, vector<int> path) {
+    int iterationNumber = 0;
+    for (int i = 0; i < path.size() - 1; i++) {
+        for (int j = 0; j < path.size(); j++) {
+            if (i != j) {
+                iterationNumber++;
+                vector<int> newpath = path;
+                swap(newpath[i], newpath[j]);
+                if (getPathWeight(graph.getGraph(), path) > getPathWeight(graph.getGraph(), newpath)) {
+                    path = newpath;
+                }
+            }
+        }
+    }
+    cout << "Number of iteration: " << iterationNumber << endl;
+    cout << "Weight: " << getPathWeight(graph.getGraph(), path) << endl;
+    cout << "Path: ";
+    for (int i : path) {
+        cout << i + 1 << " ";
+    }
+}
+
 int main() {
     int size;
     cout << "Input number of vertex: ";
     cin >> size;
     Graph graph(size);
     printGraph(graph);
-    vector<int> path = findPath(graph.getGraph(), graph.getSize());
+    vector<int> path;// = findPath(graph.getGraph(), graph.getSize());
+    for (int i = 0; i < size; i++) {
+        path.push_back(i);
+    }
     cout << "Path: ";
     for (int i : path) {
         cout << i + 1 << " ";
     }
     cout << endl;
-    getPathWeight(graph.getGraph(), path);
+    cout << "Weight: " << getPathWeight(graph.getGraph(), path) << endl;
+    changePath(graph, path);
 }
